18b21b971375fbe7f637ec4007057706ab9d5856
Hajime Senuma <hajime.senuma@gmail.com>
2021-02-11 18:05:32 +0900

    Fix some super-lint errors

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ LINK_OPTIONS = []
 def is_new_osx():
     """Check whether we're on OSX >= 10.7"""
     name = distutils.util.get_platform()
-    if sys.platform != 'darwin':
+    if sys.platform != "darwin":
         return False
     mac_ver = platform.mac_ver()[0]
     if mac_ver.startswith("10"):
-        minor_version = int(mac_ver.split('.')[1])
+        minor_version = int(mac_ver.split(".")[1])
         return minor_version >= 7
     return False
 
@@ -29,31 +29,35 @@ if is_new_osx():
     # See: https://stackoverflow.com/questions/1653047/avoid-linking-to-libstdc
     LINK_OPTIONS.append("-nodefaultlibs")
 
-mmh3module = Extension('mmh3',
-    sources = ['mmh3module.cpp', 'MurmurHash3.cpp'],
+mmh3module = Extension(
+    "mmh3",
+    sources=["mmh3module.cpp", "MurmurHash3.cpp"],
     extra_compile_args=COMPILE_OPTIONS,
     extra_link_args=LINK_OPTIONS)
 
-setup(name = 'mmh3',
-    version = '2.5.1',
-    description = 'Python wrapper for MurmurHash (MurmurHash3), a set of fast and robust hash functions.',
-    license = 'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
-    author = 'Hajime Senuma',
-    author_email = 'hajime.senuma@gmail.com',
-    url = 'http://packages.python.org/mmh3',
-    ext_modules = [mmh3module],
-    keywords = "utility hash MurmurHash",
-    long_description = open('README.rst').read(),
-    classifiers = ['Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Topic :: Software Development :: Libraries',
-    'Topic :: Utilities']
+setup(
+    name = "mmh3",
+    version="2.5.1",
+    description="Python wrapper for MurmurHash (MurmurHash3), a set of fast and robust hash functions.",
+    license="License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+    author="Hajime Senuma",
+    author_email="hajime.senuma@gmail.com",
+    url="http://packages.python.org/mmh3",
+    ext_modules=[mmh3module],
+    keywords="utility hash MurmurHash",
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
 )
