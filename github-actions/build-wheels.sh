b8ceb4f5ead5c5e2637d6db791b075b9c35a27ed
Hajime Senuma <hajime.senuma@gmail.com>
2021-02-15 15:33:52 +0900

    Test run for docker jobs

---
+++ github-actions/build-wheels.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e -u -x
+
+# From https://github.com/pypa/python-manylinux-demo
+# Originally written by Robert T. McGibbon and published under Public Domain
+
+function repair_wheel {
+    wheel="$1"
+    if ! auditwheel show "$wheel"; then
+        echo "Skipping non-platform wheel $wheel"
+    else
+        auditwheel repair "$wheel" --plat "$PLAT" -w /io/wheelhouse/
+    fi
+}
+
+# Install a system package required by our library
+yum install -y atlas-devel
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    repair_wheel "$whl"
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
+    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+done
